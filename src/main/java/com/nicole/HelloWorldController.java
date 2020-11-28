e526d64761afde14f5629504d37b182775531028
Nicoleta <nicoleta.platniuc@gmail.com>
2020-11-27 19:33:49 -0600

    First commit

---
+++ src/main/java/com/nicole/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.nicole;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	
+	//GET
+	//URI - /hello-world
+	// method - "Hello World "
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello My World!");
+	}
+}
