725a1dc35827f2f773133c36cb8253459f1ec989
Nicoleta <nicoleta.platniuc@gmail.com>
2020-11-27 21:20:02 -0600

    Enhance the service with Path variable

--- src/main/java/com/nicole/HelloWorldController.java
+++ src/main/java/com/nicole/HelloWorldController.java
@@ -1,8 +1,7 @@
 package com.nicole;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
@@ -21,5 +20,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello My World!");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		//the %s will be replace with variable "name"
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
